f0c7c95f1867415be2def6bf5370f72505ca64fe
Joaquim Neto <joaquimmy@gmail.com>
2022-03-10 02:03:36 -0300

    fix(ListItem): add list item export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Text } from './Text';
 export { default as ProgressBar } from './ProgressBar';
 export { default as List } from './List';
+export { default as ListItem } from './List/ListItem';
