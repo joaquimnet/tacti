03a670cc6f21a276bf4fdb3d80e74405031f1d47
Joaquim Neto <joaquimmy@gmail.com>
2022-03-13 08:38:10 -0300

    fix: export modal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export { default as Text } from './Text';
 export { default as ProgressBar } from './ProgressBar';
 export { default as List } from './List';
 export { default as ListItem } from './List/ListItem';
+export { default as Modal } from './Modal';
 export { default as Box } from './Box';
 export { default as Flex } from './Flex';
