ce3241218f973da1a31f97cfd2d6371bd2e87646
Joaquim Neto <joaquimmy@gmail.com>
2022-03-17 11:22:35 -0300

    feat: docs deploy

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+import tactiTheme from './tacti.theme';
+
+addons.setConfig({
+  theme: tactiTheme,
+});
---
+++ .storybook/tacti.theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Tacti',
+  brandUrl: 'https://github.com/joaquimnet/tacti',
+  brandImage: 'https://i.imgur.com/AldtCML.png',
+});
